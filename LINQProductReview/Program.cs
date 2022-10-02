7e628a24ca9c3e0195e65d6f07797f75c210c19d
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-10-02 17:22:18 +0530

    [Darshan]: add product  reviews to list

---
+++ LINQProductReview/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LINQProductReview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prodcut Review Management");
+            Console.WriteLine("Enter a Option");
+            Console.WriteLine("1.Add Values to list");
+            int option = Convert.ToInt32(Console.ReadLine());
+        
+            List<ProductReview> productReviews = new List<ProductReview>();
+            switch (option)
+            {
+                case 1:
+                    ProductReviewLINQ.AddProductReview(productReviews);
+                    break;
+                default:
+                    Console.WriteLine("Enter correct choice");
+                    break;
+
+            }   }
+    }
+}
